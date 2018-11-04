8dbb415da38b95e9620aab87dcaae15d47c629ec
jokade <jokade@karchedon.de>
2018-11-04 19:58:36 +0100

    Introduce platform config

--- src/main/scala/de/surfice/sbt/pconf/PConfPlugin.scala
+++ src/main/scala/de/surfice/sbt/pconf/PConfPlugin.scala
@@ -22,16 +22,26 @@ object PConfPlugin extends AutoPlugin {
 
     val pconfDefaultConfigPrefix: SettingKey[String] =
       settingKey[String]("Prefix for configuration files to be loaded first (can be used to enforce a default config to be loaded first)")
+
+    val pconfPlatform: SettingKey[Option[String]] =
+      settingKey[Option[String]]("If defined, load platform-specific configuration files after default config files (otherwise, no platform-specific files will be loaded)")
+
+    object PConfPlatform {
+      val Linux: Option[String] = Some("linux")
+      val MacOS: Option[String] = Some("macos")
+      val Windows: Option[String] = Some("win")
+    }
   }
 
   import autoImport._
 
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
     pconfDefaultConfigPrefix := "",
+    pconfPlatform := None,
     pconfConfigFile := baseDirectory.value / "project.conf",
 
     pconfConfigString :=
-    loadPackageConfigs((dependencyClasspath in Compile).value, pconfConfigFile.value, pconfDefaultConfigPrefix.value)
+    loadPackageConfigs((dependencyClasspath in Compile).value, pconfConfigFile.value, pconfDefaultConfigPrefix.value, pconfPlatform.value)
       .foldLeft(""){ (s,in) =>
         s + "# SOURCE: "+in._1+"\n"+
         IO.readLines(new BufferedReader(new InputStreamReader(in._2))).mkString("\n") + "\n\n"
@@ -42,22 +52,25 @@ object PConfPlugin extends AutoPlugin {
   )
 
 
-  private def loadPackageConfigs(dependencyClasspath: Classpath, projectConfig: File, defaultPrefix: String): Seq[(String,InputStream)] =
-    loadDepPackageConfigs(dependencyClasspath,defaultPrefix) ++ loadProjectConfig(projectConfig)
+  private def loadPackageConfigs(dependencyClasspath: Classpath, projectConfig: File, defaultPrefix: String, platform: Option[String]): Seq[(String,InputStream)] =
+    loadDepPackageConfigs(dependencyClasspath,defaultPrefix,platform) ++ loadProjectConfig(projectConfig)
 
   private def loadProjectConfig(projectConfig: File): Option[(String,InputStream)] =
     if(projectConfig.canRead)
       Some((projectConfig.getAbsolutePath,fin(projectConfig)))
     else None
 
-  private def loadDepPackageConfigs(cp: Classpath, defaultPrefix: String): Seq[(String,InputStream)] = {
+  private def loadDepPackageConfigs(cp: Classpath, defaultPrefix: String, platform: Option[String]): Seq[(String,InputStream)] = {
     val (dirs,jars) = cp.files.partition(_.isDirectory)
-    loadJarPackageConfigs(jars, defaultPrefix) // ++ loadDirPackageConfigs(dirs,log)
+    loadJarPackageConfigs(jars, defaultPrefix) ++
+      { if(platform.isDefined) loadJarPackageConfigs(jars,defaultPrefix,platform) else Nil }
+    //++ loadDirPackageConfigs(dirs,log)
   }
 
-  private def loadJarPackageConfigs(jars: Seq[File], defaultPrefix: String): Seq[(String,InputStream)] = {
+  private def loadJarPackageConfigs(jars: Seq[File], defaultPrefix: String, suffix: Option[String] = None): Seq[(String,InputStream)] = {
+    val file = suffix.map("package_"+_+".conf").getOrElse("package.conf")
     val files = jars
-      .map( f => (f.getName, new URL("jar:" + f.toURI + "!/package.conf").openConnection()) )
+      .map( f => (f.getName, new URL("jar:" + f.toURI + "!/" +file).openConnection()) )
       .map {
         case (f,c: JarURLConnection) => try{
           Some((f,c.getInputStream))
